ecc7ebea130a2056674ad018e9cec5cf190cfd8d
Jinesh Patel <pateljinesh118@gmail.com>
2025-01-16 12:49:56 -0600

    Add files via upload

---
+++ texasAPi/TalkTexasApplication.java
@@ -0,0 +1,13 @@
+package com.TalkTexas.texasAPi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkTexasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkTexasApplication.class, args);
+	}
+
+}
