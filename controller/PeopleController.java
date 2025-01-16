a44d556f432cfbce490d3be701a551c711752727
Jinesh Patel <pateljinesh118@gmail.com>
2025-01-16 00:43:57 -0600

    Add files via upload

---
+++ controller/PeopleController.java
@@ -0,0 +1,27 @@
+package com.TalkTexas.texasAPi.controller;
+
+import com.TalkTexas.texasAPi.service.PeopleService;
+import com.TalkTexas.texasAPi.model.PersonDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000") // Allow React frontend to access this API
+@RestController
+public class PeopleController {
+
+    @Autowired
+    private PeopleService peopleService;
+
+    // Endpoint to fetch and save data from external API
+    @GetMapping("/representatives")
+    public List<PersonDTO> fetchAndSaveTexasPeople() {
+        List<PersonDTO> texasPeople = peopleService.getTexasPeople();
+        return peopleService.saveTexasPeople(texasPeople); // Save data to the database
+    }
+
+    
+}
